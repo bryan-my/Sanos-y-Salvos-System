0fed0a77835b724f84048b72d15c2d820987fad8
Bryan <bry.jimenez@duocuc.cl>
2026-06-06 20:01:28 -0400

    Creado el ms de coincidencias

---
+++ ms-coincidencias/src/main/java/com/sanosysalvos/ms_coincidencias/MsCoincidenciasApplication.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.ms_coincidencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCoincidenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCoincidenciasApplication.class, args);
+	}
+
+}
---
+++ ms-coincidencias/src/test/java/com/sanosysalvos/ms_coincidencias/MsCoincidenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanosysalvos.ms_coincidencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCoincidenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
